a43d4bc7b48c46fe188727f4cd71ced448902814
lfuis201 <lmamania@ulasalle.edu.pe>
2022-07-08 22:36:31 -0500

    creamos la clase consumidor

---
+++ productoresconsumidores.cpp
@@ -0,0 +1,70 @@
+#include "iostream"
+#include<thread>
+#include<mutex>
+#include<deque>
+#include <condition_variable>
+using namespace std;
+// g++ productores02.cpp -lpthread
+
+
+#define NUM_PRODUCTORES 2
+condition_variable cond;
+mutex mu;
+deque<int> buffer;
+//tamaño del bufer
+const unsigned int maxBufferSize = 10;
+//void productor (int k);
+
+//clase productor
+class Productor {
+	private:
+		thread t;
+		int k;
+		void produce()
+		{
+			//numero maximo que produciran los productores e ira disminuyendo hasta 0
+			int val=100;
+			while (val)
+			{
+				unique_lock<mutex> locker(mu);
+				cond.wait(locker, []()
+				  		{ return buffer.size() < maxBufferSize; });
+				//agregamos la produccion al buffer
+				buffer.push_back(val);
+				cout << "Productor " <<k<<" produce: "<< val << endl;
+				val--;
+				locker.unlock();
+				//notificamos al otro hilo que puede continuar
+				cond.notify_one();
+			}
+		}
+	public:
+		Productor(int id) {
+			k = id;
+			t = thread(&Productor::produce, this);
+			//t.join();
+		}
+		void join_thread(){
+			t.join();
+		}
+};
+
+int main(void)
+{
+	//creamos una lista de consumidores y productores
+	Productor* productores[NUM_PRODUCTORES];
+	
+	int identificadores[NUM_PRODUCTORES];
+
+	int i;
+	//inicializamos los productores y consumidores
+	for (i=0; i<NUM_PRODUCTORES; i++)	{		
+		productores[i] = new Productor(i);
+	}
+
+	//comenzamos a producir y consumir
+	for (i=0; i<NUM_PRODUCTORES; i++)	{
+		productores[i]->join_thread();
+	}
+	return 0;
+}
